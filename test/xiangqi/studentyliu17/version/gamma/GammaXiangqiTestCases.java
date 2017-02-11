57168ac3007c34e8040010235d9de1aba6ce5135
Yang Liu <byliuyang11@gmail.com>
2017-02-11 16:26:01 -0500

    Fail createGammaXiangqiGame test

--- test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
+++ test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
@@ -1,6 +1,26 @@
 package xiangqi.studentyliu17.version.gamma;
 
+import org.junit.Before;
+import org.junit.Test;
+import xiangqi.common.XiangqiGame;
+import xiangqi.common.XiangqiGameVersion;
+import xiangqi.studentyliu17.version.XiangqiGameFactory;
+
+import static junit.framework.TestCase.assertNotNull;
+
 /**
- * Created by harryliu on 2/11/17.
+ * Test cases for gamma Xiangqi game
+ * @version Feb 11, 2016
  */
-public class GammaXiangqiGameTestCases {}
+public class GammaXiangqiTestCases {
+    private XiangqiGame game;
+    @Before
+    public void setup() {
+        game = XiangqiGameFactory.makeXiangqiGame(XiangqiGameVersion.GAMMA_XQ);
+    }
+    
+    @Test // 1
+    public void createGammaXiangqiGame() {
+        assertNotNull(game);
+    }
+}
