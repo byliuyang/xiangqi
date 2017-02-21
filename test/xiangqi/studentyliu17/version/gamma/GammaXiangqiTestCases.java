07bb79089dd302a1d65259b5f4f70f2b35e3b196
Yang Liu <byliuyang11@gmail.com>
2017-02-21 18:09:40 -0500

    Fail redAdvisorMoveDiagonallyOneStepInPalace test

--- test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
+++ test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
@@ -387,6 +387,13 @@ public class GammaXiangqiTestCases {
         assertPiece(2, 4, XiangqiColor.RED, XiangqiPieceType.NONE, XiangqiColor.NONE);
     }
     
+    @Test // 38
+    public void redAdvisorMoveDiagonallyOneStepInPalace() {
+        assertEquals(MoveResult.OK, game.makeMove(makeCoordinate(1, 4), makeCoordinate(2, 5)));
+        assertPiece(1, 4, XiangqiColor.RED, XiangqiPieceType.ADVISOR, XiangqiColor.RED);
+        assertPiece(2, 5, XiangqiColor.RED, XiangqiPieceType.NONE, XiangqiColor.NONE);
+    }
+    
     private void redSoldierCrossTheRiver() {
         assertEquals(MoveResult.OK, game.makeMove(makeCoordinate(4, 3), makeCoordinate(5,
                                                                                        3)));
