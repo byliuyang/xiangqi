6855f70cbfd8d32dda1307d6e89d9f6eef211a2c
Yang Liu <byliuyang11@gmail.com>
2017-02-21 10:33:38 -0500

    Fail redMoveSoldierForwardOneStep test

--- test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
+++ test/xiangqi/studentyliu17/version/gamma/GammaXiangqiTestCases.java
@@ -109,6 +109,11 @@ public class GammaXiangqiTestCases {
                                                                                            10)));
     }
     
+    @Test
+    public void redMoveSoldierForwardOneStep() {
+        assertEquals(MoveResult.OK, game.makeMove(makeCoordinate(4, 3), makeCoordinate(5, 3)));
+    }
+    
     private void assertHasAdvisors(int rank, XiangqiColor color) {
         assertPiece(rank, 4, XiangqiColor.RED, XiangqiPieceType.ADVISOR, color);
         assertPiece(rank, 6, XiangqiColor.RED, XiangqiPieceType.ADVISOR, color);
